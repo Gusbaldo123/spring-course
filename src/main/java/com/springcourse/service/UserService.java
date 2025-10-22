4b2b3ae76c6ed58657399d8493adadab95ed0f15
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2025-10-22 20:05:59 -0300

    Commons codec and User service
    Added:
     - Apache commons codec library
     - Hash utils to manage passwords
     - User Service

---
+++ src/main/java/com/springcourse/service/UserService.java
@@ -0,0 +1,48 @@
+package com.springcourse.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springcourse.domain.User;
+import com.springcourse.repository.UserRepository;
+import com.springcourse.service.util.HashUtil;
+
+@Service
+public class UserService {
+	@Autowired UserRepository userRepository;
+	
+	public User save(User user)
+	{
+		user.setPassword(HashUtil.getSecureHash(user.getPassword()));
+		User createdUser = userRepository.save(user);
+		return createdUser;
+	}
+
+	public User update(User user) {
+		user.setPassword(HashUtil.getSecureHash(user.getPassword()));
+		User updatedUser = userRepository.save(user);
+		return updatedUser;
+	}
+
+	public User getById(Long id) // deal with null pointer later
+	{
+		Optional<User> foundUser = userRepository.findById(id);
+		return foundUser.get();
+	}
+	
+	public List<User> listAll()
+	{
+		List<User> list = userRepository.findAll();
+		return list;
+	}
+	
+	public User login(String email, String password) // deal with null pointer later
+	{
+		String hashPassword = HashUtil.getSecureHash(password);
+		Optional<User> foundUser = userRepository.login(email, hashPassword);
+		return foundUser.get();
+	}
+}
---
+++ src/test/java/com/springcourse/service/util/HashUtilTest.java
@@ -0,0 +1,20 @@
+package com.springcourse.service.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class HashUtilTest {
+	@Test
+	@Order(1)
+	public void getSecureHashTest()
+	{
+		String password = "123";
+		String hashedPassword = HashUtil.getSecureHash(password);
+		assertThat(hashedPassword.length()).isEqualTo(64);
+	}
+}
